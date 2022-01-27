a5b3ccca4dbd172dcd69a1ae7ad2a4ee95011fd6
Dmitry1102 <meleshkevich2016@mail.ru>
2022-01-27 19:16:44 +0100

    setting screen full

---
+++ app/src/main/java/com/playsdev/firsttest/repository/Person.kt
@@ -0,0 +1,10 @@
+package com.playsdev.firsttest.repository
+
+import android.graphics.Bitmap
+
+data class Person(
+    var name:String,
+    var surname:String,
+    var date: String,
+    var image: Bitmap
+)
